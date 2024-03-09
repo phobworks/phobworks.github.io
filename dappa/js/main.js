4c5e714ed70fc7aae9a79927c4e0694ec3f2ae34
phobworks <phedwallace36th@live.com>
2024-03-09 18:59:45 +0100

    updated

--- dappa/js/main.js
+++ dappa/js/main.js
@@ -33,11 +33,36 @@ var swiper = new Swiper(".mySwiper", {
   breakpoints: {
     599: {
       slidesPerView: 2,
-      spaceBetween: 40
+      spaceBetween: 40,
     },
     1023: {
       slidesPerView: 3,
-      spaceBetween: 60
-    }
-  }
+      spaceBetween: 60,
+    },
+  },
+});
+
+/* Navbar  */
+const nav = document.querySelector(".nav__links");
+const openNavBtn = document.querySelector("#nav__toggle-open");
+const closeNavBtn = document.querySelector("#nav__toggle-close");
+
+const openNav = () => {
+  nav.style.display = "flex";
+  openNavBtn.style.display = "none";
+  closeNavBtn.style.display = "inline-block";
+};
+
+openNavBtn.addEventListener("click", openNav);
+
+const closeNav = () => {
+  nav.style.display = "none";
+  openNavBtn.style.display = "inline-block";
+  closeNavBtn.style.display = "none";
+};
+
+closeNavBtn.addEventListener("click", closeNav);
+
+nav.querySelectorAll("li a").forEach((navLink) => {
+  navLink.addEventListener("click", closeNav);
 });
