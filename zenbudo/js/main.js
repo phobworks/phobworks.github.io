c89bc8e47ae527ea1b600f590ab99fa1fed89523
phobworks <phedwallace36th@live.com>
2024-03-18 10:08:33 +0100

    Zen uploaded

---
+++ zenbudo/js/main.js
@@ -0,0 +1,59 @@
+/* ======== nav bar scroll reveal ========= */
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scrolled", window.scrollY > 0);
+});
+/* ======== nav bar scroll reveal end ========= */
+
+/* ======= Swiper Js ======= */
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  freeMode: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    599: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1023: {
+      slidesPerView: 3,
+      spaceBetween: 60,
+    },
+  },
+});
+
+/* Navbar  */
+const nav = document.querySelector(".nav__links");
+const openNavBtn = document.querySelector("#nav__toggle-open");
+const closeNavBtn = document.querySelector("#nav__toggle-close");
+
+const openNav = () => {
+  nav.style.display = "flex";
+  openNavBtn.style.display = "none";
+  closeNavBtn.style.display = "inline-block";
+};
+
+openNavBtn.addEventListener("click", openNav);
+
+const closeNav = () => {
+  nav.style.display = "none";
+  openNavBtn.style.display = "inline-block";
+  closeNavBtn.style.display = "none";
+};
+
+closeNavBtn.addEventListener("click", closeNav);
+
+nav.querySelectorAll("li a").forEach((navLink) => {
+  navLink.addEventListener("click", closeNav);
+});
+
+/* ======== Gsap ============= */
