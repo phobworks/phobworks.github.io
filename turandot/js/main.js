2ac757c8f7b9ede23038b80a439f5a990629a072
phobworks <phedwallace36th@live.com>
2024-11-25 16:17:20 +0100

    Turandot uploaded

---
+++ turandot/js/main.js
@@ -0,0 +1,44 @@
+/* Navbar  */
+const nav = document.querySelector(".nav__links");
+const openNavBtn = document.querySelector("#nav__toggle-open");
+const closeNavBtn = document.querySelector("#nav__toggle-close");
+
+const openNav = () => {
+  nav.style.display = "flex";
+  nav.classList.remove("reverse"); // Remove reverse class in case it exists
+  openNavBtn.style.display = "none";
+  closeNavBtn.style.display = "inline-block";
+};
+
+openNavBtn.addEventListener("click", openNav);
+
+const closeNav = () => {
+  // Add reverse animation class
+  nav.classList.add("reverse");
+
+  // Wait for the animation to finish before hiding the menu
+  nav.addEventListener(
+    "animationend",
+    () => {
+      nav.style.display = "none";
+      openNavBtn.style.display = "inline-block";
+      closeNavBtn.style.display = "none";
+    },
+    { once: true } // Ensures the event listener is removed after execution
+  );
+};
+
+closeNavBtn.addEventListener("click", closeNav);
+
+// Close nav on link click
+nav.querySelectorAll("li a").forEach((navLink) => {
+  navLink.addEventListener("click", closeNav);
+});
+
+/* ======== nav bar scroll reveal ========= */
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scrolled", window.scrollY > 0);
+});
+/* ======== nav bar scroll reveal end ========= */
